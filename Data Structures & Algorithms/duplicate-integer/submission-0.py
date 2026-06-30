ba530e623bfbafa62c83792589bc0061c816d0ef
Sam van Erpers Roijaards <samvanerpersroijaards@gmail.com>
2026-06-30 16:09:21 -0300

    Bulk sync: 11 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        non_dup = set()
+        for i in nums:
+            non_dup.add(i)
+        return len(non_dup) != len(nums)
+
+        
+         
---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seenNums = set()
+        for num in nums:
+            if num in seenNums:
+                return True
+            seenNums.add(num)
+        return False
